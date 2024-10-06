dd9e806ee2d4798fc185c56beb5bdd1db99452eb
shahdu <f1254489174@163.com>
2024-10-06 23:26:13 +0800

    基础代码

---
+++ TEST 01/TEST 01/test01.c
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS 
+//#include<stdio.h>
+//int main()
+//{
+//	printf("lalala\n");
+//
+//		 return 0;
+// }    
+
+//ctrl+k+c
+
+#include<stdio.h>
+//int main()
+//{
+//	printf("%d\n", 100);
+//	return 0;	
+//}
+
+
+//int main()
+//{
+//	printf("9\n", sizeof(char));
+//	printf("%zu\n", sizeof(int));
+//	printf("%zu\n", sizeof(short));
+//	printf("%zu\n", sizeof(long));
+//	printf("%zu\n", sizeof(long long));
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	short age = 19;
+//	float long = 175.5;
+//	int weight = 55; 
+//
+//	return 0;
+//
+//}
+
+
+
+//int a = 10;
+//
+//int main()
+//{
+//	int a = 90;
+//	printf("a=%d\n",a );
+//
+//	return 0;
+//}
+
+
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	
+	scanf("%d %d",&num1,&num2);
+
+	int sum = num1 + num2;
+
+	printf("%d\n", sum);
+
+	return 0;
+}
+//extern int a;
+//
+//int main()
+//{
+//	//int a = 356;
+//	{
+//		//int a = 66;
+//	    printf("a=%d\n", a);
+//	}
+//	printf("a=%d\n", a);
+//
+//	return 0;
+//} 
