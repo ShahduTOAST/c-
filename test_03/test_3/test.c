c3bc57c0cd2b280e5b518da143f642056a90228e
shahdu <f1254489174@163.com>
2024-10-12 18:31:45 +0800

    03

---
+++ test_03/test_3/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+//int num = 10;
+//int main()
+//{
+//	int num = 1;
+//	printf("num = %d\n", num);
+//	
+//	return 0;
+//}
+int main()
+{
+	//printf("\a");
+	printf("%d\n",1);
+	return 0;
+}
