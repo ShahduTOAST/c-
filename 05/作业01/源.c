c4b48cb93e2d32ff12f058b4ba12397ba47e743d
shahdu <f1254489174@163.com>
2024-10-21 22:42:17 +0800

    05 new

--- 05/作业01/源.c
+++ 05/作业01/源.c
@@ -62,14 +62,15 @@
 
 //int main()
 //{
-//	int  a = 1;
-//	float sum = 0;
-//	for (a = 1; a < 51; a=a+2)
+//	int  a = 1,b = 1;
+//	double sum = 0;
+//	for (a = 1; a < 101; a=a++)
 //	{
-//		sum = (1.0 / a) - (1.0 / (a + 1));
-//		sum += sum;
+//		/*sum = (1.0 / a) - (1.0 / (a + 1));*/
+//		sum = sum + 1.0 / a * b;
+//		b = -b;
 //	}
-//	printf("sum = %f\n", sum);
+//	printf("sum = %lf\n", sum);
 //	return 0;
 //
 //}
@@ -87,21 +88,66 @@
 //}
 
 
-int main()
-{
-	int i = 0,n = 0;
-	for (i = 9; i <= 100; i++)
-	{
-		n = i / 9;
-		while (i / 9)
-		{
-			n = (i / 10) / 9;
-			n += n;
-		}
-		n += n;
-		
-	}
-	printf("%d\n",n);
-	return 0;
-	
-}
+//int main()
+//{
+//	int i = 0,n = 0;
+//	for (i = 9; i <= 100; i++)
+//	{
+//		n = i / 9;
+//		while (i / 9)
+//		{
+//			n = (i / 10) / 9;
+//			n += n;
+//		}
+//		n += n;
+//		
+//	}
+//	printf("%d\n",n);
+//	return 0;
+//	
+//}
+
+
+//int main()
+//{
+//	int arr[10] = { 0,1,2,3,4,5,6,7,8,9 };
+//	int i = 0;
+//	int max = 0;
+//	for (i = 0; i <= 9; i++)
+//	{
+//		if (max < arr[i])
+//		{
+//			max = arr[i];
+//		}
+//	}
+//	printf("max number is %d\n", i);
+//	printf("%d", max);
+//
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//	int a = 0, i = 0, j = 2;
+//	for (i = 100; i < 201; i++)
+//	{
+//		int b = 1;
+//		for (j = 2; j < i; j++)
+//		{
+//			a = i % j ;
+//			if (a == 0)
+//			{
+//				b = 0;
+//				break;
+//			}
+//		}
+//		if (b == 1)
+//		{
+//			printf("%d\n", i);
+//		}
+//		
+//	}
+//	return 0;
+//}
+
